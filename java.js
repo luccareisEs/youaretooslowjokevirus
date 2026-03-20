08b9b6366b18159d4983aaf3aeed55f9e592acee
luccareisEs <lucca.reis@aln.senaicimatec.edu.br>
2026-03-20 14:52:24 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var randtop = randomIntFromInterval(1, 1500);
+var randleft = randomIntFromInterval(1, 1500);
+
+function yaai(){
+    var randtop = randomIntFromInterval(1, 1500);
+    var randleft = randomIntFromInterval(1, 1500);
+    
+    var features = `width=300,height=220,top=${randtop},left=${randleft}`;
+    
+    window.open('page2.html', 'tooslow' + new Date().getTime(), features);
+} 
---
+++ java3.js
@@ -0,0 +1,18 @@
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var randtop = randomIntFromInterval(1, 1500);
+var randleft = randomIntFromInterval(1, 1500);
+
+function yaai(){
+    var randtop = randomIntFromInterval(1, 1500);
+    var randleft = randomIntFromInterval(1, 1500);
+    
+    var features = `width=300,height=220,top=${randtop},left=${randleft}`;
+    
+    window.open('page2.html', 'tooslow' + new Date().getTime(), features);
+} 
+const myAudio = new Audio('tooslowaudio.mp3');
+myAudio.loop = true;
+myAudio.play();
